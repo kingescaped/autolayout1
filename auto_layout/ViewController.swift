e95b2e9640d8cda4441c8d9b3d48f2c45b6e876e
Lionel Vinh Quang <90240183+kingescaped@users.noreply.github.com>
2022-04-28 09:36:29 +0700

    autolayout 1

--- auto_layout/ViewController.swift
+++ auto_layout/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            let splashVC = SplashViewController()
+            splashVC.modalPresentationStyle = .fullScreen
+            present(splashVC, animated: false, completion: nil)
+        }
 
 }
 
